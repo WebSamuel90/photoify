a7d24e57bf5caf90c0c772faf437fe35758cdbc6
Samuel <sumlebulle@hotmail.com>
2018-12-13 12:57:50 +0100

    change in database, fullname to name

--- app/users/create.php
+++ app/users/create.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-if (isset($_POST['username'], $_POST['fullname'], $_POST['email'], $_POST['password'])) {
+if (isset($_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'])) {
   $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
-  $fullName = filter_var(trim($_POST['fullname']), FILTER_SANITIZE_STRING);
+  $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
   $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
   $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 
-  $query = "INSERT INTO users (username, fullname, email, password) VALUES(:username, :fullName, :email, :password)";
+  $query = "INSERT INTO users (username, name, email, password) VALUES(:username, :name, :email, :password)";
 
-    // INSERT INTO users (username, fullname, email, password) VALUES ('test', 'samuel', 'samuel0911@hotmail.com', '12345');
-    $statement = $pdo->prepare($query);
+  $statement = $pdo->prepare($query);
 
     if (!$statement) {
       die(var_dump($pdo->errorInfo()));
@@ -23,7 +22,7 @@ if (isset($_POST['username'], $_POST['fullname'], $_POST['email'], $_POST['passw
 
 
   $statement->bindParam(':username', $username, PDO::PARAM_STR);
-  $statement->bindParam(':fullName', $fullName, PDO::PARAM_STR);
+  $statement->bindParam(':name', $name, PDO::PARAM_STR);
   $statement->bindParam(':email', $email, PDO::PARAM_STR);
   $statement->bindParam(':password', $password, PDO::PARAM_STR);
 
