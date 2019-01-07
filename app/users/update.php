c4c4c4b63098146e30fa1b953006d50a4134de89
Samuel <sumlebulle@hotmail.com>
2019-01-07 13:06:16 +0100

    Dont remember...

--- app/users/update.php
+++ app/users/update.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+if (isset($_SESSION['user']['username'])){
+  $email = $_SESSION['user']['username'];
+  $statement = $pdo->prepare('SELECT * FROM users WHERE username = :username');
+  // $statement = $pdo->prepare('SELECT * FROM users');
+  if(!$statement){
+    die(var_dump($pdo->errorInfo()));
+  }
+  $statement->bindParam(':email', $email, PDO::PARAM_STR);
+  $statement->execute();
+  $currentusers = $statement->fetch(PDO::FETCH_ASSOC);
+}
+
 if (isset($_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'])) {
   $username = filter_var(trim($_POST['username']), FILTER_SANITIZE_STRING);
   $name = filter_var(trim($_POST['name']), FILTER_SANITIZE_STRING);
@@ -12,16 +24,8 @@ if (isset($_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'
 
   $id = $_SESSION['user']['id'];
 
-  // $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
-  // $statement->bindParam(':id', $id, PDO::PARAM_STR);
-  // $statement->execute();
-  // $update = $statement->fetch(PDO::FETCH_ASSOC);
 
-
-  // $query = "INSERT INTO users (username, name, email, password) VALUES(:username, :name, :email, :password)";
-
-
-  $query = "UPDATE users SET username = :username, name = :name, email = :email, password = :password WHERE id = $id";
+  $query = "UPDATE users SET username = :username, name = :name, email = :email, password = :password WHERE id = :id";
   // $query = "UPDATE users SET name = :name WHERE id = :id";
 
   $statement = $pdo->prepare($query);
@@ -31,7 +35,7 @@ if (isset($_POST['username'], $_POST['name'], $_POST['email'], $_POST['password'
     }
 
 
-  // $statement->bindParam(':id', $id, PDO::PARAM_STR);
+  $statement->bindParam(':id', $id, PDO::PARAM_INT);
   $statement->bindParam(':username', $username, PDO::PARAM_STR);
   $statement->bindParam(':name', $name, PDO::PARAM_STR);
   $statement->bindParam(':email', $email, PDO::PARAM_STR);
